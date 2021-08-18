828d77481e53bd0fdb727e145023b8cb575c22b3
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-18 18:31:23 -0300

    webcomponent criado com shadow DOM

---
+++ star-rating-webcomponents/StarRater.js
@@ -0,0 +1,75 @@
+class StarRater extends HTMLElement {
+  constructor() {
+    super()
+    this.build()
+  }
+  
+  build() {
+    const shadow = this.attachShadow({mode: 'open' })
+    
+    shadow.appendChild(this.styles())
+    
+    const rater = this.createRater()
+    this.stars = this.createStars()
+    
+    this.stars.forEach(star => rater.appendChild(star))
+    this.resetRating()
+    
+    shadow.appendChild(rater)
+  }
+  
+  createRater() {
+    const rater = document.createElement('div')
+    rater.classList.add('star-rater')
+    rater.addEventListener('mouseout', this.resetRating.bind(this))
+    return rater
+  }
+  
+  createStars() {
+    const createStar = (_, id) => {
+      const star = document.createElement('span')
+      star.classList.add('star')
+      star.setAttribute('data-value', Number(id) + 1)
+      star.innerHTML = '&#9733;'
+      
+      star.addEventListener('click', this.setRating.bind(this))
+      star.addEventListener('mouseover', this.ratingHover.bind(this))
+      return star
+    }
+    
+    return Array.from({ length: 5 }, createStar)
+  }
+  
+  resetRating() {
+    this.currentRatingValue = this.getAttribute('data-rating') || 0
+    this.hightLightRating()
+  }
+  
+  ratingHover(event) {
+    this.currentRatingValue = event.currentTarget.getAttribute('data-value')
+    this.hightLightRating()
+  }
+  
+  hightLightRating() {
+    this.stars.forEach(star => {
+      star.style.color = this.currentRatingValue >= star.getAttribute('data-value')
+      ? 'yellow'
+      : 'gray'
+    })
+  }
+  
+  setRating(event) {
+    this.setAttribute('data-rating', event.currentTarget.getAttribute('data-value'))
+  }
+  
+  styles() {
+    const style = document.createElement('style')
+    style.textContent = `.star { 
+    font-size:5rem;
+    cursor:pointer;
+  }`
+    return style
+  }
+}
+
+customElements.define('star-rater', StarRater)
